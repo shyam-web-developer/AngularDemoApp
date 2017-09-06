5b365ea13c90df94b18c434b3d749d6392d5eb39
shyam-web-developer <shyamyadav.sb2005@gmail.com>
2017-09-06 10:52:24 +0530

    bbvb

---
+++ src/app/components/Root.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './Header.jsx'
+import Main from './Main.jsx'
+
+const Root = () => (
+  <div>
+    <Header />
+    <Main />
+  </div>
+)
+
+export default Root
---
+++ src/app/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import Root from './components/Root.jsx';
+
+render((
+  <BrowserRouter>
+    <Root />
+  </BrowserRouter>
+), document.getElementById('root'));
